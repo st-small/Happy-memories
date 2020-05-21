f6a2323e20dcb4a52f4527c27fb575bdbe94360f
Станислав Шияновский <st-small@rambler.ru>
2020-05-21 18:47:34 +0300

    Initial Commit

---
+++ Счатливые денёчки/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Счатливые денёчки
+//
+//  Created by Stanly Shiyanovskiy on 21.05.2020.
+//  Copyright © 2020 Stanly Shiyanovskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
